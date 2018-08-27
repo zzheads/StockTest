6c9f46aeb38103a008031f26d9bd047a73610975
zzheads <zzheads@gmail.com>
2018-08-27 10:45:51 +0300

    Initial Commit

---
+++ StockTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StockTest
+//
+//  Created by Алексей Папин on 27.08.2018.
+//  Copyright © 2018 Алексей Папин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
